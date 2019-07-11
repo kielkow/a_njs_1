0a8ce84d9f5a686fe6924b9e7535911a37ce3a3e
kielkow <matheuskiel@fiorifer.com.br>
2019-07-10 22:15:29 -0300

    Add marko var data

--- src/app/routes/rotas.js
+++ src/app/routes/rotas.js
@@ -1,4 +1,4 @@
-module.exports = (app) =>{
+module.exports = (app) => {
     app.get('/', function (req, resp) {
         resp.send(
             `
@@ -17,10 +17,21 @@ module.exports = (app) =>{
             `
         )
     })
-    
+
     app.get('/lista', function (req, resp) {
         resp.marko(
-            require('../views/livros/lista/lista.marko')
+            require('../views/livros/lista/lista.marko'), {
+                livros: [
+                    {
+                        id: 1,
+                        titulo: 'Fundamentos Nodejs'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Nodejs Avançado'
+                    }
+                ]
+            }
         )
     }
     )
