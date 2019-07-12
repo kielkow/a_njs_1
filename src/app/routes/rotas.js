849305d295cf134eadff50e976eb08fea0d2fdf0
kielkow <matheuskiel@fiorifer.com.br>
2019-07-12 19:50:56 -0300

    Add form and route post

--- src/app/routes/rotas.js
+++ src/app/routes/rotas.js
@@ -37,4 +37,12 @@ module.exports = (app) => {
             )
 
     })
+
+    app.get('/livros/form', function(req, resp){
+        resp.marko(require('../views/livros/form/form.marko'))
+    })
+
+    app.post('/livros', function(req, resp){
+        console.log(req.body)
+    })
 }
