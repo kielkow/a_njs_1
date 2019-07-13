01aa8437f79b86bc1b7756570515c3f882722152
kielkow <matheuskiel@fiorifer.com.br>
2019-07-12 21:10:01 -0300

    Add function adiciona in livro-dao.js and finish app.post

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -17,10 +17,29 @@ class LivroDao {
         })
     }
 
-    adiciona(){
+    adiciona(livro) {
         return new Promise((resolve, reject) => {
-            this._db.all(
-                ''
+            this._db.run(
+                `
+                INSERT INTO LIVROS(
+                    titulo,
+                    preco,
+                    descricao
+                ) values (?, ?, ?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err){
+                    if (err) {
+                        console.log(err)
+                        return reject('Nao foi possivel cadastrar livro')
+                    }
+
+                    resolve()
+                }
             )
         })
     }
